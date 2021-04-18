4420e85e48b212a32995a153453dadf95c72d787
Neverlastinger <shadow_87@mail.bg>
2021-04-18 20:33:06 +0300

    Improves walkthrough screen UI:

--- src/screens/WalkthroughScreen.js
+++ src/screens/WalkthroughScreen.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { Image, Dimensions, ActivityIndicator } from 'react-native';
+import { Image, Dimensions, ActivityIndicator, StatusBar } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import styled from 'styled-components/native';
 import Swiper from 'react-native-swiper';
 import { STORAGE_KEYS } from '~/const';
+import logoIcon from '~/assets/logo-full.png';
 import imageOne from '~/assets/walkthrough/walkthrough-1.png';
 import imageTwo from '~/assets/walkthrough/walkthrough-2.png';
 import imageThree from '~/assets/walkthrough/walkthrough-3.png';
@@ -39,39 +40,57 @@ const WalkthroughScreen = ({ navigation }) => {
   }
 
   return (
-    <SwiperWrapper
-      loop={false}
-      onIndexChanged={onIndexChanged}
-      activeDotColor={BRAND_COLOR_BLUE}
-      scrollEnabled={scrollEnabled}
-    >
-      <Slide>
-        <SlideImage source={imageOne} />
-        <Description>{t('walkthroughSlide1')}</Description>
-      </Slide>
-      <Slide>
-        <SlideImage source={imageTwo} />
-        <Description>{t('walkthroughSlide2')}</Description>
-      </Slide>
-      <Slide>
-        <SlideImage source={imageThree} />
-        <Description>{t('walkthroughSlide3')}</Description>
-      </Slide>
-      <LastSlide>
-        <Description>{t('walkthroughSlideLast')}</Description>
-        <ActivityIndicator />
-      </LastSlide>
-    </SwiperWrapper>
+    <Screen>
+      <StatusBar backgroundColor="black" barStyle="dark-content" />
+      <TopBar>
+        <Logo source={logoIcon} />
+      </TopBar>
+      <SwiperWrapper
+        loop={false}
+        onIndexChanged={onIndexChanged}
+        activeDotColor={BRAND_COLOR_BLUE}
+        scrollEnabled={scrollEnabled}
+      >
+        <Slide>
+          <Description>{t('walkthroughSlide1')}</Description>
+          <SlideImage source={imageOne} />
+        </Slide>
+        <Slide>
+          <Description>{t('walkthroughSlide2')}</Description>
+          <SlideImage source={imageTwo} />
+        </Slide>
+        <Slide>
+          <Description>{t('walkthroughSlide3')}</Description>
+          <SlideImage source={imageThree} />
+        </Slide>
+        <LastSlide>
+          <Description>{t('walkthroughSlideLast')}</Description>
+          <ActivityIndicator />
+        </LastSlide>
+      </SwiperWrapper>
+    </Screen>
   );
 };
+
+const Screen = styled.View`
+  flex: 1;
+`;
+
+const TopBar = styled.View`
+  padding-top: 40px;
+  flex-basis: 130px;
+  height: 130px;
+  background-color: black;
+`;
 
 const SwiperWrapper = styled(Swiper)`
 `;
 
 const Slide = styled.View`
   flex: 1;
-  justify-content: space-around;
+  justify-content: center;
   align-items: center;
+  padding-bottom: 60px;
   background-color: #f2f2f2;
 `;
 
@@ -80,19 +99,26 @@ const LastSlide = styled(Slide)`
 `;
 
 const SlideImage = styled(Image)`
-  width: ${Dimensions.get('window').width}px;
+  width: ${0.9 * Dimensions.get('window').width}px;
   height: undefined;
   max-height: 80%;
   aspectRatio: 1;
 `;
 
 const Description = styled.Text`
-  margin-bottom: 18px;
   padding: 18px 36px;
   text-align: center;
   line-height: 22px;
   font-size: 16px;
-  color: #444;
+  font-weight: bold;
+  color: black
+`;
+
+const Logo = styled.Image`
+  align-self: center;
+  width: 80%;
+  height: undefined;
+  aspectRatio: 4.1;
 `;
 
 export default WalkthroughScreen;
